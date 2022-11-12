c9d63234ce8207d4c28c4a37bd10b3c054ca5115
aspde <1678566105@qq.com>
2022-11-12 23:43:25 +0800

    补充题目

---
+++ StackQueue/src/main/java/Program1047.java
@@ -0,0 +1,27 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 删除字符串中的所有相邻重复项
+ */
+public class Program1047 {
+
+    public String removeDuplicates(String s) {
+
+        // 使用StringBuffer作为栈
+        StringBuffer res = new StringBuffer();
+        int top = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (top >= 0 && res.charAt(top) == c) {
+                res.deleteCharAt(top);
+                top--;
+            } else {
+                res.append(c);
+                top++;
+            }
+        }
+        return res.toString();
+    }
+}
