b8a87e0774eb779620dcbf490564e6f7d9faf7ae
aspde <1678566105@qq.com>
2023-03-02 06:50:35 +0800

    单调栈题目

--- MonotonicStack/src/main/java/Program42.java
+++ MonotonicStack/src/main/java/Program42.java
@@ -1,6 +1,28 @@
+import java.util.ArrayDeque;
 
 /**
  * 接雨水
  */
 public class Program42 {
+
+    // 单调栈解法
+    public int trap(int[] height) {
+
+        int sum = 0;
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < height.length; i++) {
+            while(!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                int middle = height[stack.pop()];
+                if(!stack.isEmpty()) {
+                    int high = Math.min(height[stack.peek()], height[i]) - middle;
+                    int width = i - stack.peek() - 1;
+                    sum += high * width;
+                }
+            }
+            stack.push(i);
+        }
+
+        return sum;
+    }
 }
--- MonotonicStack/src/main/java/Program84.java
+++ MonotonicStack/src/main/java/Program84.java
@@ -1,6 +1,35 @@
+import java.util.ArrayDeque;
 
 /**
  * 柱状图中最大的矩形
  */
 public class Program84 {
+
+    // 单调栈解法
+    public int largestRectangleArea(int[] heights) {
+
+        int result = 0;
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        int [] newHeights = new int[heights.length + 2];
+        newHeights[0] = 0;
+        newHeights[newHeights.length - 1] = 0;
+        for (int index = 0; index < heights.length; index++){
+            newHeights[index + 1] = heights[index];
+        }
+        heights = newHeights;
+
+        for (int i = 0; i < heights.length; i++) {
+            while(!stack.isEmpty() && heights[i] < heights[stack.peek()]) {
+                int high = heights[stack.pop()];
+                if(!stack.isEmpty()) {
+                    int width = i - stack.peek() - 1;
+                    result = Math.max(result, high * width);
+                }
+            }
+            stack.push(i);
+        }
+
+        return result;
+    }
 }
