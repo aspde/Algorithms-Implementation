c14a1229335f7c119b04b67b476a05a3c3df124f
aspde <1678566105@qq.com>
2023-03-03 07:47:23 +0800

    树题目

--- BinaryTree/src/main/java/Program101.java
+++ BinaryTree/src/main/java/Program101.java
@@ -1,6 +1,28 @@
+import sun.reflect.generics.tree.Tree;
 
 /**
  * 对称二叉树
  */
 public class Program101 {
+
+    public boolean isSymmetric(TreeNode root) {
+        return compare(root.left, root.right);
+    }
+
+    private boolean compare(TreeNode left, TreeNode right) {
+
+        if(left == null && right != null) {
+            return false;
+        } else if (left != null && right == null) {
+            return false;
+        } else if (left == null && right == null) {
+            return true;
+        } else if (left.val != right.val) {
+            return false;
+        }
+
+        boolean outside = compare(left.left, right.right);
+        boolean inside = compare(left.right, right.left);
+        return outside && inside;
+    }
 }
--- BinaryTree/src/main/java/Program111.java
+++ BinaryTree/src/main/java/Program111.java
@@ -3,4 +3,22 @@
  * 二叉树的最小深度
  */
 public class Program111 {
+
+    public int minDepth(TreeNode root) {
+
+        if(root == null) {
+            return 0;
+        }
+
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+
+        if(root.left == null) {
+            return rightDepth + 1;
+        }
+        if(root.right == null) {
+            return leftDepth + 1;
+        }
+        return Math.min(leftDepth, rightDepth) + 1;
+    }
 }
